884d277ab00fa9992ba8a08d03d68394098660b5
bv-erika <vargae@iit.uni-miskolc.hu>
2022-09-12 13:18:23 +0200

    proba

--- README.md
+++
@@ -1,1 +0,0 @@
-# Elso_het
--- elso.c
+++ elso.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(int argc, char const *argv[]){
 
-    printf("Hello wolrd\n");
+// Single line comment
+/*
+ Multi
+ Line
+ Comment
+*/
+
+int main(int argc, char const *argv[])
+{
+ 
+    double r;
+
+    printf("Radius: ");
+    scanf("%lf",&r);
+    double circumfence =2*r*M_PI;
+    double area = pow(r,2)*M_PI;
+    printf("pi: %lf A: %lf C: %lf",M_PI,area,circumfence);
+
     return 0;
-
 }
